759ec38068de53bd5d06bedf59f69b336fdf4dd9
hels15 <helsgithub15@gmail.com>
2023-06-04 14:03:01 +0100

    code formatted, meta feature, colormap youth stage.

--- slodon/slodonix/display/display.py
+++ slodon/slodonix/display/display.py
@@ -1,23 +1,25 @@
 """Basic display"""
 
-import types
 import os
+from slodon.slodonix.protocol.display import BaseDisplay
 
 
-def open_display(name=os.environ['DISPLAY']):
-    """
-    Open a display
-
-    Returns a display object
-    """
-    return Display(name=name)
-
-
-class Display:
+class Display(BaseDisplay):
     """
     Represents a basic display
 
     A set of screens for a single user with one keyboards and one pointer(usually a mouse) is called a display.
     """
-    def __init__(self, name):
-        pass
+    def __init__(self, display) -> None:
+        super().__init__(display=display)
+
+
+def open_display(name=os.environ['DISPLAY']) -> Display:
+    """
+    Open a display
+
+    name: The name of the display
+
+    Returns a display object
+    """
+    return Display(display=name)
--- slodon/slodonix/protocol/connect.py
+++ slodon/slodonix/protocol/connect.py
@@ -5,6 +5,8 @@ import re
 import os
 from typing import Type
 import uuid
+from uuid import UUID
+
 # This project
 from slodon.slodonix.utils.func import is_socket_connected
 
@@ -14,20 +16,25 @@ SUPPORTED_PROTOCOLS = (None, "tcp", "unix")
 
 class _OPEN:
     """Opened connection"""
+
     pass
 
 
 class _CLOSE:
     """Closed Connection"""
+
     pass
 
 
 class _STATUS:
     """EITHER _OPEN or _CLOSE"""
+
     STATUS = Type[_OPEN] | Type[_CLOSE]
 
 
-DISPLAY_RE = re.compile(r'^((?P<proto>tcp|unix)/)?(?P<host>[-:a-zA-Z0-9._]*):(?P<dno>[0-9]+)(\.(?P<screen>[0-9]+))?$')
+DISPLAY_RE = re.compile(
+    r"^((?P<proto>tcp|unix)/)?(?P<host>[-:a-zA-Z0-9._]*):(?P<dno>[0-9]+)(\.(?P<screen>[0-9]+))?$"
+)
 
 
 def get_tcp(address: str, dno: int) -> socket.SocketType:
@@ -38,7 +45,6 @@ def get_tcp(address: str, dno: int) -> socket.SocketType:
     :return: TCP socket connection to address
     """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("in case of tcp, address is: ", address)
     s.connect((address, 6000 + dno))
     return s
 
@@ -64,13 +70,13 @@ def get_display(display) -> tuple[str, str, str, int, int]:
         if m is not None:
             protocol, host, dno, screen = [
                 m.groupdict().get(field, defaults.get(field))
-                for field in ('proto', 'host', 'dno', 'screen')
+                for field in ("proto", "host", "dno", "screen")
             ]
             break
     else:
         raise DisplayNameError(display)
 
-    if protocol == 'tcp' and not host:
+    if protocol == "tcp" and not host:
         # Host is mandatory when protocol is TCP.
         raise DisplayNameError(display)
 
@@ -85,22 +91,21 @@ def get_display(display) -> tuple[str, str, str, int, int]:
 
 def get_socket(display) -> socket.SocketType:
     """
-         Make a connection to a display
+    Make a connection to a display
     """
-    print("display", display)
     display, protocol, host, dno, screen = display
 
     assert protocol in SUPPORTED_PROTOCOLS, f"Protocol {protocol} not supported"
 
     try:
         # TCP socket, note the special case: `unix:0.0` is equivalent to `:0.0`.
-        if (protocol is None or protocol != 'unix') and host and host != 'unix':
+        if (protocol is None or protocol != "unix") and host and host != "unix":
             s = get_tcp(host, dno)
         else:
-            address = '/tmp/.X11-unix/X%d' % dno
+            address = "/tmp/.X11-unix/X%d" % dno
             if not os.path.exists(address):
                 # Use abstract address.
-                address = '\0' + address
+                address = "\0" + address
 
             try:
                 s = get_unix(address)
@@ -118,7 +123,15 @@ def get_socket(display) -> socket.SocketType:
 
 class Connection:
     """Connection to a display"""
-    CONNECTIONS = 0  # Number of connections
+
+    class Meta:
+        """Hold metadata about the connection"""
+
+        def __init__(self, **args) -> None:
+            self.data = args
+
+        def __str__(self) -> str:
+            return str(self.data)
 
     def __init__(self, display: str = None) -> None:
         """
@@ -130,20 +143,36 @@ class Connection:
         - None
         """
 
-        Connection.CONNECTIONS += 1  # Increment number of connections
-        self._pure_display = display
-        self._display_info = get_display(display)
-        self._connection = self._make_connection(self._display_info)
-        self._status = is_socket_connected(self._connection)
-        self._protocol = self._display_info[2]
-        self._id = uuid.uuid4()
+        self._pure_display: str = (
+            display  # the actual basic display name(without any processing)
+        )
+        self._display_info: tuple = get_display(display)  # the display info(tuple)
+        self._connection: socket.SocketType = self._make_connection(
+            self._display_info
+        )  # make the connection
+        self._status: bool = is_socket_connected(
+            self._connection
+        )  # check if the connection is opened or closed
+        self._protocol: str = self._display_info[2]  # protocol as a string
+        self._id: UUID = uuid.uuid4()  # unique id for the connection
+
+        self.meta = self.Meta(
+            pure_display=self._pure_display,
+            display_info=self._display_info,
+            connection=self._connection,
+            status=self._status,
+            protocol=self._protocol,
+            id=self._id,
+        )
 
     # noinspection PyMethodMayBeStatic
     def _make_connection(self, display_info) -> socket.SocketType:
         return get_socket(display_info)
 
+    @property
     def status(self) -> _STATUS.STATUS:
         """
+        -read only property
         Return back the current status of the connection
         :return: _OPEN or _CLOSE
         """
@@ -159,6 +188,10 @@ class Connection:
         for i in range(len(keys)):
             _text += f"{keys[i]}={self._display_info[i]} || "
         return _text
+
+    @property
+    def display_info(self):
+        return self._display_info
 
     def info(self) -> tuple:
         return self._display_info
