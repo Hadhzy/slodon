c6a563af06866c4673b31109a4d02545374c7de0
hels15 <helsgithub15@gmail.com>
2023-06-21 10:57:35 +0100

    Extend main Windows file with `get_os` and `Info` class. Also added docs-strings.

--- slodon/slodonix/slodonix/slodonix_windows.py
+++ slodon/slodonix/slodonix/slodonix_windows.py
@@ -1,51 +1,130 @@
-from ctypes import windll as w
 # https://www.win7dll.info/user32_dll.html
+from ctypes import windll as w
+
+# This project
+from slodon.slodonix.systems.windows.keyboard_map import key_map
+
+__all__ = ["Display", "get_os"]
+
+
+class Screen:
+    """
+    Represent a screen in a display on windows, it contains information about the screen.
+    """
+    pass
 
 
 class _Interact:
-    def __init__(self):
+    """
+
+    """
+    def __init__(self) -> None:
+        """
+
+        """
         pass
 
-    def key_up(self, key):
-        pass
+    def key_up(self, key) -> None:
+        """
+         key release
+         ### Arguments
+            - key (str): The key to release
+         ### Returns
+            - None
+        """
 
-    def key_down(self, key):
-        pass
+    def key_down(self, key) -> None:
+        """
+        Key press without release
+        ### Arguments
+          - key (str): The key to press down
+        ### Returns
+          - None
+        """
 
-    def position(self):
-        pass
+    def position(self) -> None:
+        """
+        x-y position of the mouse
+        ### Arguments
+          - key (str): Return back the mouse position
+        ### Returns
+          - None
+        """
 
-    def size(self):
+    def screen(self):
+        """
+
+        """
         pass
 
     def moveto(self):
-        pass
+        """
+        x-y position of the mouse
+        ### Arguments
+         - key (str): Return back the mouse position
+        ### Returns
+         - None
+         """
 
     def mouse_down(self):
+        """
+
+        """
         pass
 
     def mouse_up(self):
+        """
+
+        """
         pass
 
     def click(self):
+        """
+
+        """
         pass
 
     def mouse_is_swapped(self):
+        """
+
+        """
         pass
 
     def send_mouse_event(self):
+        """
+
+        """
         pass
 
     def scrool(self):
+        """
+
+        """
         pass
 
     def hscrool(self):
+        """
+
+        """
+        pass
+
+
+class _Info:
+    """
+    Returns back information about the display
+    """
+
+    def get_top_window(self):
         pass
 
 
 class Display:
+    """
+
+    """
     def __init__(self):
         self.interact = _Interact()
+        self.info = _Info()
 
     def __enter__(self):
         pass
@@ -54,3 +133,8 @@ class Display:
         pass
 
 
+def get_os() -> str:
+    """
+    Return back the currently used operating system.
+    """
+    return "Windows"
