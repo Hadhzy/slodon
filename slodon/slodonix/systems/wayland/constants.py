ba34ded5c0c971f0cbbdaa08db51b94e218bfd44
hels15 <helsgithub15@gmail.com>
2023-07-16 21:43:58 +0100

    Remove / Add wayland by using CLI.

--- slodon/slodonix/systems/wayland/constants.py
+++ slodon/slodonix/systems/wayland/constants.py
@@ -4,4 +4,4 @@ __all__ = ["WAYLAND_SITE", "DOWNLOAD_TO", "CURRENT_PATHS", "URL"]
 WAYLAND_SITE = "https://gitlab.freedesktop.org/wayland/wayland"
 DOWNLOAD_TO = "~/lib_wayland"
 CURRENT_PATHS = DOWNLOAD_TO  # by default
-URL = ""
+URL = "https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.32/downloads/wayland-protocols-1.32.tar.xz"
