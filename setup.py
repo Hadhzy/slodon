b5f970fa7abe07b8df5bd22f9f1ed8eba0d44149
gaborhadhazy <gaborhadhazy07@gmail.com>
2023-08-27 15:43:57 +0100

    changed build backend (setuptools.meta)

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="hzy",
+    python_requires=">=3.11",
+    description="Emulated input for Wayland in python",
+    long_description="Emulated input for Wayland in python",
+    url="https://github.com/Hadhzy/hzy",
+    license="Apache 2.0",
+    packages=["hzy"],
+    author="Hadhzy Organisation"
+)
